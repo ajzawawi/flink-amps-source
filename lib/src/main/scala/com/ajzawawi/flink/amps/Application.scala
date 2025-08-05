ccd41354a15beccea75a94e286fa4c1383332572
AJ Zawawi <aminojzawawi@gmail.com>
2025-08-04 21:21:01 -0400

    Add libs

--- lib/src/main/scala/com/ajzawawi/flink/amps/Application.scala
+++
@@ -1,7 +0,0 @@
-package com.ajzawawi.flink.amps
-
-object Application {
-  def main(args: Array[String]): Unit = {
-
-  }
-}
---
+++ lib/src/main/scala/com/ajzawawi/flink/amps/FlinkAMPSJob.scala
@@ -0,0 +1,10 @@
+package com.ajzawawi.flink.amps
+/**
+ * Example job that runs the AMPS source locally using Flink's StreamExecutionEnvironment.
+ * For development or smoke testing.
+ */
+object FlinkAMPSJob {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
